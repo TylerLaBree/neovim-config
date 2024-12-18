6980e7bd6dfb1994ab4062605b3d73aab7f50a80
Tyler LaBree <tylerlabree@github.com>
2024-12-18 15:05:42 -0600

    fix: change load order

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 local using_gnome = vim.env.DESKTOP_SESSION
 
+require "user.lazy"
 if using_gnome then
     require "user.style"
 else
     require "user.style_simple"
 end
-require "user.lazy"
 require "user.options"
 require "user.keymaps"
 require "user.cmp"
